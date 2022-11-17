e9c0b6bc170ef6a69cbd1ecf2e8d488eb5ed7783
MuhammedAlkhudiryAqar <mohammad@aqar.fm>
2022-11-17 16:49:45 +0300

    [fix] use FlatList for better performance

--- src/screens/list/index.tsx
+++ src/screens/list/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScrollView} from 'react-native';
+import {FlatList} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 import ListData from '../../utils/fake-data';
@@ -20,9 +20,15 @@ export interface IListItem {
 const ListScreen = () => {
   return (
     <SafeAreaView edges={['top', 'bottom']}>
-      <ScrollView contentContainerStyle={{paddingHorizontal: 16}}>
-        {ListData.map(item => <ListItem key={item.id} item={item} />)}
-      </ScrollView>
+      <FlatList
+        data={ListData}
+        renderItem={({item}) => <ListItem item={item} />}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          // in real project, I would be using specific colors, but for the sake of simplicity, I'm using this
+          backgroundColor: '#f9f9f9',
+        }}
+      />
     </SafeAreaView>
   );
 };
